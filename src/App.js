a3b70d4923630fa784fa938ae2c6ed5f372b1b7a
Kirsten S <kirsten@Kirstens-MacBook-Air.local>
2020-06-25 20:11:16 -0400

    refactored Products.js to use context instead of props

--- src/App.js
+++ src/App.js
@@ -21,12 +21,12 @@ function App() {
 
 	return (
 		<div className="App">
-			<ProductContext.Provider value={{products: products, addItem: addItem}}>
+			<ProductContext.Provider value={{products, addItem}}>
 				<Navigation cart={cart} />
 
 				{/* Routes */}
 				<Route exact path="/">
-					<Products products={products} addItem={addItem} />
+					<Products />
 				</Route>
 
 				<Route path="/cart">
